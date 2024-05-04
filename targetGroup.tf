f6f2c59364ea1a76c1623d42af124d8d51ebf64e
CodingManoj <CodingManoj@gmail.com>
2024-05-03 21:48:09 -0400

    Auto Push

--- targetGroup.tf
+++ targetGroup.tf
@@ -1,6 +1,6 @@
 locals {
     INSTANCE_COUNT = var.OD_INSTANCE_COUNT + var.SPOT_INSTANCE_COUNT
-    INSTANCE_IDS   = concat(aws_spot_instance_request.spot.*.id , aws_instance.od.*.id)
+    INSTANCE_IDS   = concat(aws_spot_instance_request.spot.*.spot_instance_id , aws_instance.od.*.id)
 }
 
 
