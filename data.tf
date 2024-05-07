34a8c3df8fb42e7731d24cb950cb79329406493d
CodingManoj <CodingManoj@gmail.com>
2024-05-06 23:46:23 -0400

    Auto Push

--- data.tf
+++ data.tf
@@ -14,3 +14,13 @@ data "aws_ami" "ami" {
   name_regex       = "centos8-with-ansible"     # Give the AMI ID of the AMI Created with Ansible Installed ( Use the base image as centos-8 lab image )
   owners           = ["355449129696"]           # give your aws account number 
 }
+
+# datasource that reads the info from alb statefile 
+data "terraform_remote_state" "alb" {
+  backend = "s3"
+  config = {
+    bucket = "b57-tf-state-bucket"
+    key    = "dev/alb/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
