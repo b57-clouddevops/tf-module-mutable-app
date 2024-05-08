18ebcff1a68079f03a568ea05fbbc02c6bfdb6db
CodingManoj <CodingManoj@gmail.com>
2024-05-07 21:35:29 -0400

    Auto Push

--- targetGroup.tf
+++ targetGroup.tf
@@ -4,6 +4,15 @@ resource "aws_lb_target_group" "app" {
   port     = var.APP_PORT
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  health_check {
+    path                = "/health"         # Path to check for health (e.g., a health endpoint) 
+    healthy_threshold   = 2                 # Minimum number of successful checks before marking healthy
+    unhealthy_threshold = 2                 # Maximum number of failed checks before marking unhealthy
+    timeout             = 4                 # Timeout in seconds for each health check attempt
+    interval            = 5 
+    enabled             = true 
+  }
 }
 
 # Register the Backend Instance to the appropriate target group.check " 
