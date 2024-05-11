a52ad7b508d220fad273d6e797ab3b64041552b0
CodingManoj <CodingManoj@gmail.com>
2024-05-10 22:18:44 -0400

    Auto Push

--- listenerRules.tf
+++ listenerRules.tf
@@ -26,7 +26,7 @@ resource "aws_lb_listener_rule" "prv_app_rule" {
 #  public Listener Rule
 resource "aws_lb_listener_rule" "pub_app_rule" {
   count        = var.INTERNAL  ? 0 : 1 
-  listener_arn = data.terraform_remote_state.alb.outputs.PUBLIC_LISTENER_ARN
+  listener_arn = data.terraform_remote_state.alb.outputs.PUBLIC_LISTENER_ARN[0]
   priority     = random_integer.priority.result
 
   action {
